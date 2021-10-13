20a19f2abcd7afa977c0284846d7982d4b60e69d
Mamuritto <mamurdilshodov@icloud.com>
2021-10-14 03:31:38 +0500

    add js file in html

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-const a = "another"
-const b = "Our first lesson"
+var title = "mySecondLesson";
+var screens = "Simple, Hard, Interactive";
+var screenPrice = 11;
+var rollback = 17;
+var fullPrice = 500;
+var adaptive = true;
+var num = 266219;
+console.log(typeof title); 
+console.log(typeof fullPrice); 
+console.log(typeof adaptive); 
+console.log(screens.length); 
+console.log("Cost of site screen " + screenPrice + " $"); 
+console.log("Cost of full site " + fullPrice + " $"); 
+console.log(screens.toLowerCase());
+console.log(screens.split(" "));
 
-const logged = () => {
-    console.log("we can commit");
-} 
